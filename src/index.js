dcd4ab84eb1eeb21e38426fd7f4c4932a3bfb2a5
beerpoldet <poladet.a@gmail.com>
2021-08-11 12:25:26 +0700

    Add port var

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ app.post("/verify", (req, res) => {
   res.end();
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Application is running at ${port}`);
 });
