d02d3be2771b5ddfd0580831b7dd22762cb61195
beerpoldet <poladet.a@gmail.com>
2021-08-11 15:23:14 +0700

    Try iframe

--- public/button.js
+++ public/button.js
@@ -1,18 +1,22 @@
 const user_token = "fake_token";
 const host = "https://cookies-seven.vercel.app";
 
-function verityToken() {
-  return fetch(host + "/api/verify-token", {
+function issueToken() {
+  return fetch(host + "/api/issue-token", {
     headers: {
       "Content-Type": "application/json",
     },
     method: "POST",
+    body: JSON.stringify({ user_token }),
   });
 }
 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("COOKIE", document.cookie);
-  issueToken().then(() => {
-    console.log("verified");
-  });
+  document.querySelector("#launch_message_spring").addEventListener('click', () => {
+    issueToken().then(() => {
+      console.log("verified");
+    });
+  })
 });
+
