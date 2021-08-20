b7f436a0f40a4587e149ff0fbdb69ebec555a5ed
beerpoldet <poladet.a@gmail.com>
2021-08-20 15:39:18 +0700

    Add mock authorized team member

--- src/index.js
+++ src/index.js
@@ -11,8 +11,10 @@ app.use("/", express.static(path.resolve("public")));
 app.use(bodyParser.json());
 
 // const openAPIURL = "https://open-api-dev.ifyoucan.com"
-// const openAPIURL = "http://localhost:3000";
-const openAPIURL = "https://munission-demo.vercel.app";
+const openAPIURL =
+  process.env.NODE_ENV !== "production"
+    ? "http://localhost:3000"
+    : "https://munission-demo.vercel.app";
 
 const apiClient = {
   issueOneTimeToken: (apiKey, username) =>
@@ -26,17 +28,15 @@ const apiClient = {
 };
 
 app.get("/api/messagespring/:apiKey/:username", (req, res) => {
-  console.log(JSON.stringify(process.env))
   apiClient
     .issueOneTimeToken(req.params.apiKey, req.params.username)
     .then((response) => {
       if (!response.ok) {
         return res.redirect("/error.html");
       }
-      return response.json()
-        .then(json => {
-          res.redirect("/success.html?" + json.token);
-        })
+      return response.json().then((json) => {
+        res.redirect("/success.html?" + json.token);
+      });
     });
 });
 
@@ -48,6 +48,21 @@ app.post("/v1/api/token", (req, res) => {
   }
   res.json({
     token: "one-time-token",
+  });
+});
+
+app.post("/v1/api/team-member", (req, res) => {
+  if (req.body.token !== "one-time-token") {
+    return res.status(401).json({
+      error: "Unauthorized",
+    });
+  }
+  return res.json({
+    accessToken:
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjg4MCwiaWF0IjoxNjI5NDQ4NDkzLCJleHAiOjE2Mjk0NTIwOTMsInN1YiI6IkFjY2Vzc1Rva2VuIn0.jZT7Ltq3eW_Hggpeh72A1eVX-_g96gjkYp1xWQ-JNhA",
+    refreshToken:
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjg4MCwiaWF0IjoxNjI5MTAxOTYzLCJleHAiOjE2MzE4MDE5NjMsInN1YiI6IlJlZnJlc2hUb2tlbiJ9.cwL5nimHv5fp1ojFWmO6cktYpFEbra9AqQl8pjQ3BxI",
+    expiresIn: 3600,
   });
 });
 
